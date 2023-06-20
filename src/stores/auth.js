db75e6bbff154e6231f04dd325ca1ce7a22b3165
DavidGalileo24 <david.salgado.mejia@dive.digital>
2023-06-20 17:48:27 -0600

    Fixed login data and added token in localstorage

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -5,24 +5,31 @@ import axios from 'axios';
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     user: [],
-    formUser: {
-      email: '',
-      password: '',
-    },
-    
+    token: '',
+    user_data: ''
   }),
-  actions: {
-    login(data){
-      console.log(data);
-      axios.post('/auth/login', {email: data.email, password: data.password})
-      .then(response => {
-        console.log(response);
-      })
-      
+    actions: {
+      async login(data){
+        await axios.post('/auth/login', {email: data.email, password: data.password})
+        .then(response => {
+          this.user_data = response.data.user,
+          this.token = response.data.token;
+          localStorage.setItem('token', JSON.stringify(this.token));
+          localStorage.setItem('user', JSON.stringify(this.user_data));
+          this.router.push('/competitor');
+        })
+        .catch(err => {
+          console.log(err);
+        })
+      },
+      async authUser(){
+        await axios.post('/user-data', {headers: { 'Authorization': `Bearer ${this.token}` }})
+        .then(response => {
+          this.user_data = response.data.data;
+        })
+        .error(err => {
+          console.log(err);
+        });
     },
-    async getAuth(){
-      const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
-      this.user = response.data;
-    }
-  },
+  }
 })
