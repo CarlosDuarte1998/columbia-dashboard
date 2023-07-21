d63411241e82f7e83a222d486a7c3273d592119e
Carlos Alexander Duarte Hernadez <64160994+CarlosDuarte1998@users.noreply.github.com>
2023-07-21 17:58:43 -0600

    Correccion de exportacion

--- src/view/CompetitorDash.vue
+++ src/view/CompetitorDash.vue
@@ -2,11 +2,11 @@
 import { ref, onMounted } from 'vue'
 import { ModalsContainer } from 'vue-final-modal'
 import NavbarDashboard from '../components/NavbarDashboard.vue';
-import SectionCompetitors from '../components/sectionCompetitors.vue';
+import sectionCompetitors from '../components/sectionCompetitors.vue';
 
 </script>
 
 <template>
     <NavbarDashboard />
-    <SectionCompetitors />
+    <sectionCompetitors />
 </template>
